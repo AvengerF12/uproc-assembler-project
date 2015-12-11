016536ff4f51b6df2803a66ac76016e92f2c9fe8
Antonello Dettori <tonello96'gmail.com>
2015-12-11 01:29:06 +0000

    Initial commit of the parser... It probably doesn't work

---
+++ sheetParser.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <zip.h>
+
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxml/xmlreader.h>
+
+
+char *read_zip_content(char *);
+static void print_element_names(xmlNode * a_node);
+
+
+int main(int argc, char *argv[])
+{
+    char *xml_content = read_zip_content(argv[1]);
+
+    xmlDoc *doc = xmlReadMemory(xml_content, strlen(xml_content) * sizeof(char) + 1, "content.xml", NULL, 0);
+
+    xmlTextReaderPtr reader = xmlReaderWalker(doc);
+
+    xmlNode *root_element = NULL;
+
+    root_element = xmlDocGetRootElement(doc);
+
+    print_element_names(root_element);
+
+    xmlFreeDoc(doc);
+
+    xmlCleanupParser();
+
+
+    return 0;
+}
+
+
+static void print_element_names(xmlNode * a_node)
+{
+        xmlNode *cur_node = NULL;
+
+    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
+        if (cur_node->type == XML_TEXT_NODE) {
+            printf("node type: Element, name: %s\n", cur_node->name);
+        }
+
+        print_element_names(cur_node->children);
+    }
+}
+
+
+char *read_zip_content(char *zip_name)
+{
+    int err = 0;
+    zip_t *z_file = zip_open(zip_name, ZIP_RDONLY, &err);
+    if(z_file == NULL) {fputs("Cannot open zip file", stderr);}
+
+    char *f_to_read = "content.xml";
+
+    struct zip_stat content_stat;
+
+    int f_stat_r = zip_stat(z_file, f_to_read, 0, &content_stat);
+    if(f_stat_r == -1) {fputs("Cannot get stats", stderr);}
+
+    zip_file_t *f_content = zip_fopen(z_file, f_to_read, ZIP_FL_UNCHANGED);
+    if(f_content == NULL) {fputs("Cannot open content file", stderr);}
+
+    char *content_data = malloc(sizeof(char)*content_stat.size + 1);
+
+    zip_fread(f_content, content_data, content_stat.size);
+
+    int f_close_r = zip_fclose(f_content);
+    if(f_close_r == -1) {fputs("Some error occurred while closing the file.", stderr);};
+
+    int close_r = zip_close(z_file);
+    if(close_r == -1){fputs("Some error occurred while closing the zip file.", stderr);};
+
+    return content_data;
+}
