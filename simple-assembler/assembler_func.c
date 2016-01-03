3f558533ac08a3a56fb76f1f50ebdd8fae6fa8c1
Antonello Dettori <dettori.an@gmail.com>
2016-01-03 15:53:09 +0100

    Converted the functions to work in 8 bits... since the cpu is 8bit

--- simple-assembler/assembler_func.c
+++ simple-assembler/assembler_func.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int file_write_b(char *o_file_name, int *buffer, size_t size);
-void* safe_realloc (void* ptr, size_t new_size);
-int append_int_buffer(int **old_buffer, int *to_append_buffer, int buffer_size, int append_size);
+int file_write_b(char *o_file_name, int8_t *buffer, size_t size, size_t count);
+void *safe_realloc (void* ptr, size_t new_size);
+int append_int_buffer(int8_t **old_buffer, int8_t *to_append_buffer, int *buffer_size, int append_size);
 
 
-int file_write_b(char *o_file_name, int *buffer, size_t size)
+int file_write_b(char *o_file_name, int8_t *buffer, size_t size, size_t count)
 {
     FILE * o_file = NULL;
 
     o_file = fopen (o_file_name, "wb+");
     if (o_file == NULL) {fputs ("File error", stderr); exit(4);}
 
-    fwrite(buffer, sizeof(int), size, o_file);
+    fwrite(buffer, size, count, o_file);
 
     fclose(o_file);
 
@@ -21,9 +21,9 @@ int file_write_b(char *o_file_name, int *buffer, size_t size)
 }
 
 
-void* safe_realloc (void* ptr, size_t new_size)
+void *safe_realloc (void* ptr, size_t new_size)
 {
-	int *temp_ptr = realloc(ptr, sizeof(int*)*new_size);
+	void *temp_ptr = realloc(ptr, new_size);
 
 	if(temp_ptr == NULL){
 		fprintf(stderr, "Unexpected behaviour: realloc");
@@ -35,20 +35,18 @@ void* safe_realloc (void* ptr, size_t new_size)
 }
 
 
-int append_int_buffer(int **old_buffer, int *to_append_buffer, int buffer_size, int append_size)
+int append_int_buffer(int8_t **old_buffer, int8_t *to_append_buffer, int *buffer_size, int append_size)
 {
     // Allocate space required for all the new values
-    *old_buffer = safe_realloc(*old_buffer, (buffer_size+append_size)*sizeof(int));
-
-    int temp_size = buffer_size;
+    *old_buffer = safe_realloc(*old_buffer, (*buffer_size+append_size)*sizeof(int));
 
     // Add all the new values one at time
     for(int i=0;i<append_size;i++){
-        *old_buffer[temp_size] = to_append_buffer[i];
+        (*old_buffer)[*buffer_size] = to_append_buffer[i];
 
-        printf("SUP: %X %d\n", *old_buffer[temp_size], temp_size);
+//        printf("SUP: %X %d\n", (*old_buffer)[*buffer_size], *buffer_size);
 
-        temp_size++;
+        (*buffer_size)++;
 
     }
 
--- simple-assembler/assembler_func.h
+++ simple-assembler/assembler_func.h
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 
-int file_write_b(char *o_file_name, int *buffer, size_t size);
-void* safe_realloc (void* ptr, size_t new_size);
-int append_int_buffer(int **old_buffer, int *to_append_buffer, int buffer_size, int append_size);
-
+int file_write_b(char *o_file_name, int8_t *buffer, size_t size, size_t count);
+void *safe_realloc (void* ptr, size_t new_size);
+int append_int_buffer(int8_t **old_buffer, int8_t *to_append_buffer, int *buffer_size, int append_size);
